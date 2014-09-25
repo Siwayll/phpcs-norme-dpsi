54305657b7565ae56a47c7360b8ed381bb1569e2
Adrien <aimbert@solire.fr>
2014-09-25 16:50:50 +0200

    Correction notice sur l'absence de commentaire

--- Sniffs/Commenting/FunctionCommentSniff.php
+++ Sniffs/Commenting/FunctionCommentSniff.php
@@ -448,7 +448,7 @@ class DPSI_Sniffs_Commenting_FunctionCommentSniff implements PHP_CodeSniffer_Sni
                 }
 
 
-                if ($paramComment[0] != strtoupper($paramComment[0])) {
+                if (isset($paramComment[0]) && $paramComment[0] != strtoupper($paramComment[0])) {
                     $error = 'Le commentaire de "%s" doit commencer par une majuscule';
                     $data  = array($paramName);
                     $this->currentFile->addError($error, $errorPos, 'MissingParamCommentUpper', $data);
